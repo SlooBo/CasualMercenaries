1ac8482b8a06e9a153c00d61dfd36f3add62200f
havyton <tti13smiikas@kamk.fi>
2015-06-15 14:18:23 +0300

    Enabled networking code for project and testing if creating & joining session works

--- Source/CasualMercenaries/CasualMercenaries.Build.cs
+++ Source/CasualMercenaries/CasualMercenaries.Build.cs
@@ -6,21 +6,31 @@ public class CasualMercenaries : ModuleRules
 {
 	public CasualMercenaries(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
+            "OnlineSubsystem",
+			"OnlineSubsystemUtils", });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+        DynamicallyLoadedModuleNames.AddRange(
+                 new string[] {
+				"OnlineSubsystemNull",
+				"NetworkReplayStreaming",
+				"NullNetworkReplayStreaming",
+				"HttpNetworkReplayStreaming"
+			                    }
+            );
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
+        PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+        {
+            if (UEBuildConfiguration.bCompileSteamOSS == true)
+            {
+                DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+            }
+        }
 	}
 }
