d5fa00a8ba497a64f71f263735255a3e260b60f0
havyton <tti13smiikas@kamk.fi>
2015-06-15 16:59:44 +0300

    Added required build data

--- Source/CasualMercenaries/CasualMercenaries.Build.cs
+++ Source/CasualMercenaries/CasualMercenaries.Build.cs
@@ -8,9 +8,10 @@ public class CasualMercenaries : ModuleRules
 	{
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
             "OnlineSubsystem",
-			"OnlineSubsystemUtils", });
+			"OnlineSubsystemUtils", 
+            "Slate","SlateCore","UMG",
+        });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
         DynamicallyLoadedModuleNames.AddRange(
                  new string[] {
@@ -21,7 +22,7 @@ public class CasualMercenaries : ModuleRules
 			                    }
             );
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
         PrivateDependencyModuleNames.Add("OnlineSubsystem");
