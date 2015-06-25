5c28fbb22dc6dfda0dbfdcf14b26940ef6d8a9a2
havyton <tti13smiikas@kamk.fi>
2015-06-25 12:43:51 +0300

    Adding more server browser functionality

--- Source/CasualMercenaries/ServerBrowserLogic.cpp
+++ Source/CasualMercenaries/ServerBrowserLogic.cpp
@@ -8,6 +8,8 @@
 
 UServerBrowserLogic::UServerBrowserLogic() :Super()
 {
+	OnFindSessionsCompleteDelegate = FOnFindSessionsCompleteDelegate::CreateUObject(this, &UServerBrowserLogic::OnFindSessionsComplete);
+	searchingSessions = false;
 }
 UServerBrowserLogic::~UServerBrowserLogic()
 {
@@ -32,7 +34,23 @@ void UServerBrowserLogic::SetUp(UUserWidget *widget,UWorld *world)
 			createSessionButton = tempCreateSessionButton;
 			createSessionButton->OnClicked.AddDynamic(this, &UServerBrowserLogic::CreateSession);
 		}
-		
+		UScrollBox *tempServerListScrollBox = Cast<UScrollBox>(children[i]);
+		if (tempServerListScrollBox != nullptr && tempServerListScrollBox->GetName().Equals("ServerListScrollBox"))
+		{
+			serverListScrollBox = tempServerListScrollBox;
+		}
+		UButton *tempFindSessionsButton = Cast<UButton>(children[i]);
+		if (tempFindSessionsButton != nullptr && tempFindSessionsButton->GetName().Equals("FindSessionsButton"))
+		{
+			findSessionsButton = tempFindSessionsButton;
+			findSessionsButton->OnClicked.AddDynamic(this, &UServerBrowserLogic::FindSessions);
+		}
+		UButton *tempAddSession = Cast<UButton>(children[i]);
+		if (tempAddSession != nullptr && tempAddSession->GetName().Equals("AddSessionButton"))
+		{
+			addSessionButton= tempAddSession;
+			addSessionButton->OnClicked.AddDynamic(this, &UServerBrowserLogic::AddSessionToGUI);
+		}
 	}
 }
 void UServerBrowserLogic::CreateSession()
@@ -47,6 +65,68 @@ void UServerBrowserLogic::CreateSession()
 	testSettings.bAllowJoinViaPresence = true;
 
 	auto tt = world->GetUniqueID();
-	bool succesfull = session->CreateSession(tt, "TestSession", testSettings);
+	bool succesfull = session->CreateSession(tt, "Game", testSettings);
 	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Did we create session: " + FString::FromInt(succesfull));
+}
+void UServerBrowserLogic::FindSessions()
+{
+	if (searchingSessions)
+		return;
+	IOnlineSubsystem *test = IOnlineSubsystem::Get();
+	IOnlineSessionPtr session = test->GetSessionInterface();
+	auto tt = world->GetUniqueID();
+
+	SearchSettings = MakeShareable(new FOnlineSessionSearch());
+	SearchSettings->MaxSearchResults = 8;
+	SearchSettings->bIsLanQuery = true;
+	SearchSettings->TimeoutInSeconds = 20;
+	TSharedRef<FOnlineSessionSearch> SearchSettingsRef = SearchSettings.ToSharedRef();
+	OnFindSessionsCompleteDelegateHandle = session->AddOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegate);
+	auto hosted = session->FindSessions(tt, SearchSettingsRef);
+	searchingSessions = true;
+}
+void UServerBrowserLogic::OnFindSessionsComplete(bool bWasSuccessful)
+{
+	IOnlineSubsystem *test = IOnlineSubsystem::Get();
+	IOnlineSessionPtr session = test->GetSessionInterface();
+	auto tt = world->GetUniqueID();
+	searchingSessions = false;
+	UE_LOG(LogOnlineGame, Verbose, TEXT("OnFindSessionsComplete bSuccess: %d"), bWasSuccessful);
+
+	IOnlineSubsystem* const OnlineSub = IOnlineSubsystem::Get();
+	if (OnlineSub)
+	{
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+		if (Sessions.IsValid())
+		{
+			Sessions->ClearOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegateHandle);
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Results: " + FString::FromInt(SearchSettings->SearchResults.Num()));
+			searchResults = SearchSettings.Get()->SearchResults;
+			for (int32 SearchIdx = 0; SearchIdx < SearchSettings->SearchResults.Num(); SearchIdx++)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Test");
+				const FOnlineSessionSearchResult& SearchResult = SearchSettings->SearchResults[SearchIdx];
+				//SearchResult.Session.SessionInfo.Get()->
+
+				DumpSession(&SearchResult.Session);
+			}
+
+			OnFindSessionsComplete().Broadcast(bWasSuccessful);
+		}
+	}
+}
+void UServerBrowserLogic::JoinSession()
+{
+	IOnlineSubsystem *test = IOnlineSubsystem::Get();
+	IOnlineSessionPtr session = test->GetSessionInterface();
+	auto tt = world->GetUniqueID();
+	session->JoinSession(tt, TEXT("Game") , searchResults[0]);
+}
+void UServerBrowserLogic::AddSessionToGUI()
+{
+	UButton *newButton = ConstructObject<UButton>(UButton::StaticClass());
+	UTextBlock *newText = ConstructObject<UTextBlock>(UTextBlock::StaticClass());
+	newText->SetText(FText::FromString("This is server"));
+	newButton->AddChild(newText);
+	serverListScrollBox->AddChild(newButton);
 }
--- Source/CasualMercenaries/ServerBrowserLogic.h
+++ Source/CasualMercenaries/ServerBrowserLogic.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "UILogicBase.h"
+#include "Online.h"
 #include "ServerBrowserLogic.generated.h"
 
 /**
@@ -19,9 +20,27 @@ public:
 	void SetUp(UUserWidget *widget,UWorld *world);
 	UFUNCTION()
 	void CreateSession();
+	UFUNCTION()
+	void FindSessions();
+	UFUNCTION()
+	void JoinSession();
+	UFUNCTION()
+	void AddSessionToGUI();
 private:
+	TArray<FOnlineSessionSearchResult, FDefaultAllocator> searchResults;
+	bool searchingSessions;
 	UWorld *world;
 	UUserWidget *widget;
 	UButton *createSessionButton;
-	
+	UButton *findSessionsButton;
+	UButton *addSessionButton;
+	UScrollBox *serverListScrollBox;
+	TSharedPtr<FOnlineSessionSearch> SearchSettings;
+
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
+	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
+
+	FOnFindSessionsComplete& OnFindSessionsComplete() { return FindSessionsCompleteEvent; }
+	FOnFindSessionsComplete FindSessionsCompleteEvent;
 };
