53392cd10c4b7abac4b46d3c4fcfc3c4972bfcd5
Ari Vuollet <ari.vuollet@kapsi.fi>
2015-06-30 15:35:43 +0300

    Add respawn mode AtGhost (spawns player at spectator location), warmup refactoring (+1 squashed commits)
    Squashed commits:
    
    [f1a958c] Add respawn mode AtGhost (spawns player at spectator location), warmup refactoring

--- Source/CasualMercenaries/CMGameMode.cpp
+++ Source/CasualMercenaries/CMGameMode.cpp
@@ -6,6 +6,7 @@
 #include "PlayerCharacter.h"
 #include "Util.h"
 #include "PlayerHud.h"
+#include "CMSpectator.h"
 
 ACMGameMode::ACMGameMode(const FObjectInitializer& objectInitializer)
 	: Super(objectInitializer)
@@ -23,7 +24,7 @@ ACMGameMode::ACMGameMode(const FObjectInitializer& objectInitializer)
 	//HUDClass = APlayerHud::StaticClass();
 	//PlayerControllerClass = ACMPlayerController::StaticClass();
 	PlayerStateClass = ACMPlayerState::StaticClass();
-	//SpectatorClass = ACMSpectatorClass::StaticClass();
+	SpectatorClass = ACMSpectator::StaticClass();
 	//GameStateClass = ACMGameState::StaticClass();
 
 	DefaultPlayerName = FText::FromString("OfficeRat");
@@ -54,31 +55,47 @@ bool ACMGameMode::ShouldSpawnAtStartSpot(AController* player)
 
 AActor* ACMGameMode::ChoosePlayerStart_Implementation(AController* player)
 {
-	APlayerStart* best = NULL;
-	TArray<APlayerStart*> spawns;
+	AActor* spawnLocation = NULL;
 
-	for (TActorIterator<APlayerStart> iter(GetWorld()); iter; ++iter)
-		spawns.Add(*iter);
+	if (respawnMode == RespawnMode::AtGhost)
+	{
+		APlayerController* playerController = player->CastToPlayerController();
+		if (playerController != NULL)
+		{
+			spawnLocation = playerController->GetSpectatorPawn();
+		}
+	}
+	
+	if (respawnMode == RespawnMode::AtSpawnPoint ||
+		(respawnMode == RespawnMode::AtGhost && spawnLocation == NULL))
+	{
+		APlayerStart* best = NULL;
+		TArray<APlayerStart*> spawns;
 
-	// TODO: choose spawn point which is furthest away from any enemy player
+		for (TActorIterator<APlayerStart> iter(GetWorld()); iter; ++iter)
+			spawns.Add(*iter);
 
-	// choose the random spawn
-	int random = FMath::RandRange(0, spawns.Num() - 1);
-	if (spawns.IsValidIndex(random))
-		best = spawns[random];
+		// TODO: choose spawn point which is furthest away from any enemy player
 
-	if (best == NULL)
-		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Error: No spawn points were found, add PlayerStart to level"));
+		// choose the random spawn
+		int random = FMath::RandRange(0, spawns.Num() - 1);
+		if (spawns.IsValidIndex(random))
+			best = spawns[random];
 
-	return (AActor*)best;
+		if (best == NULL)
+			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Error: No spawn points were found, add PlayerStart to level"));
+
+		spawnLocation = (AActor*)best;
+	}
+
+	return spawnLocation;
 }
 
 void ACMGameMode::HandleMatchIsWaitingToStart()
 {
 	Super::HandleMatchIsWaitingToStart();
 
-	inGameState = InGameState::Warmup;
-	OnWarmupStart();
+	inGameState = InGameState::WarmupStarting;
 
 	waitElapsed = -1;
 	GetWorld()->GetTimerManager().SetTimer(waitTimer, this, &ACMGameMode::WaitTickSecond, 1.0f, true, 0.0f);
@@ -86,40 +103,56 @@ void ACMGameMode::HandleMatchIsWaitingToStart()
 
 void ACMGameMode::StartMatch()
 {
+	if (inGameState != InGameState::Warmup)
+		MatchState = MatchState::WaitingToStart;
+
 	Super::StartMatch();
 
-	if (inGameState != InGameState::Warmup)
+	if (inGameState == InGameState::Warmup)
 	{
-		GetWorld()->GetTimerManager().ClearTimer(waitTimer);
+		OnWarmupStart();
+		return;
+	}
+
+	GetWorld()->GetTimerManager().ClearTimer(waitTimer);
 	
-		// match needs restarting, move players back to spawn
-		for (FConstPlayerControllerIterator iter = GetWorld()->GetPlayerControllerIterator(); iter; ++iter)
-		{
-			if ((*iter)->GetPawn() != NULL)
-				(*iter)->GetPawn()->Reset();
+	// match needs restarting, move players back to spawn
+	for (auto iter = GetWorld()->GetPlayerControllerIterator(); iter; ++iter)
+	{
+		if ((*iter)->GetPawn() != NULL)
+			(*iter)->GetPawn()->Reset();
 
-			ACMPlayerState* playerState = static_cast<ACMPlayerState*>((*iter)->PlayerState);
-			if (playerState != NULL)
-				playerState->ResetStats();
+		ACMPlayerState* playerState = static_cast<ACMPlayerState*>((*iter)->PlayerState);
+		if (playerState != NULL)
+			playerState->ResetStats();
 
-			RespawnPlayer(*iter);
-		}
+		RespawnPlayer(*iter);
 	}
 
 	mapTimeElapsed = 0;
 	GetWorld()->GetTimerManager().SetTimer(mapTimerHandle, this, &ACMGameMode::MapTickSecond, 1.0f, true);
 
-	OnStartMatch();
+	OnMatchStart();
 }
 
 void ACMGameMode::WaitTickSecond()
 {
 	waitElapsed++;
 
-	int32 numPlayers = GetNumPlayers();
+	if (inGameState == InGameState::WarmupStarting)
+	{
+		if (waitElapsed >= 0)
+		{
+			// actually starts warmup and not the real match
+			inGameState = InGameState::Warmup;
+			StartMatch();
+		}
+	}
 
 	if (inGameState == InGameState::Warmup)
 	{
+		int32 numPlayers = GetNumPlayers();
+
 		// ready to start the match or warmup
 		if (numPlayers >= minPlayersToStart)
 		{
@@ -136,7 +169,8 @@ void ACMGameMode::WaitTickSecond()
 				+ TEXT(" ( ") + FString::FromInt(numPlayers) + TEXT("/") + FString::FromInt(minPlayersToStart) + TEXT(" )"));
 		}
 	}
-	else if (inGameState == InGameState::Starting)
+
+	if (inGameState == InGameState::Starting)
 	{
 		if (waitElapsed >= startTime)
 		{
@@ -183,7 +217,7 @@ int32 ACMGameMode::MapTimeElapsed()
 
 void ACMGameMode::OnWarmupStart_Implementation()
 {
-	StartMatch();
+
 }
 
 void ACMGameMode::OnPlayerDeath_Implementation(APlayerController* player, APlayerController* killer)
@@ -243,12 +277,13 @@ void ACMGameMode::RestartPlayer(AController* controller)
 		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Error: RestartPlayer controller is not PlayerController"));
 		return;
 	}
-	
+
+	Super::RestartPlayer(controller);
+
 	player->PlayerState->bIsSpectator = false;
 	player->ChangeState(NAME_Playing);
 	player->ClientGotoState(NAME_Playing);
-
-	Super::RestartPlayer(controller);
+	
 	Cast<APlayerCharacter>(player->GetPawn())->ServerInitInventory();
 }
 
